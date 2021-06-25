f9466e1c75f1c5a2e5cb1aa0bc2c90c5c6e3729f
hideakiyamamoto <61575280+ideahy@users.noreply.github.com>
2021-06-25 09:06:21 +0900

    before bagfix

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState(["AAAAA", "IIIII"]);
-  const [completeTodos, setCompleteTodos] = useState(["UUUUU"]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
@@ -49,6 +49,7 @@ export const App = () => {
         />
         <button onClick={onClickAdd}>追加</button>
       </div>
+
       <div className="incomplete-area">
         <p className="title">未完了のtodo</p>
         <ul>
@@ -63,6 +64,7 @@ export const App = () => {
           })}
         </ul>
       </div>
+
       <div className="complete-area">
         <p className="title">完了のtodo</p>
         <ul>
@@ -70,19 +72,12 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button
-                  onClick={() => {
-                    onClickBack(index);
-                  }}
-                >
-                  戻す
-                </button>
+                <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             );
           })}
         </ul>
       </div>
-      <div></div>
     </>
   );
 };
